ca4fe8daaca05686af3bfd0c0b6ef0e1b2f7c527
Omar <alhait.omar@gmail.com>
2024-01-25 11:14:38 -0800

    hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
 import { Route, Routes } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Help from "./Help";
 import Home from "./Home";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/help" Component={Help} />
-      <Route path="/" Component={Home} />
-    </Routes>
+    <HashRouter>
+      {/* <div
+        style={{
+          position: "absolute",
+          top: 0,
+          width: "100vw",
+          height: "5vh",
+          borderWidth: 2,
+          borderColor: "green",
+        }}
+      >
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/help">Help</a>
+            </li>
+          </ul>
+        </nav>
+      </div> */}
+      <Routes>
+        <Route path="/help" element={<Help />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </HashRouter>
   );
 };
 
